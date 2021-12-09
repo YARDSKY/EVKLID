de1b660c5aefaa4a1c82c5cd24b0eeb939bc1e16
yardsky-sergey <46065135+YARDSKY@users.noreply.github.com>
2021-12-09 23:30:50 +0500

    FAQ has been completed

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  $( ".js-accordion" ).accordion({
-    collapsible: true,
-    active: false,
-    icons: false,
-    heightStyle: 'content'
-  });
-})
