9318d75a032d84fcd5e5e2640f81f9198281db05
Sadovskii Sergei <46065135+YARDSKY@users.noreply.github.com>
2022-01-23 17:28:52 +0500

    MVP homework
    burger's working

--- js/script.js
+++ js/script.js
@@ -14,3 +14,27 @@ $( ".js-accordion" ).accordion({
   icons: false,
   heightStyle: 'content'
 });
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#burger").addEventListener('click', function () {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#burger1").addEventListener('click', function () {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#btn1").addEventListener('click', function () {
+    document.querySelector('#search').classList.toggle('is-active')
+  })
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#btn2").addEventListener('click', function () {
+    document.querySelector('#search').classList.toggle('is-active')
+  })
+})
