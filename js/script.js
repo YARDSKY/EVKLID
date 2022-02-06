f011b3c1a4db6c1afdeab5d1179b755f68292dc9
Sadovskii Sergei <46065135+YARDSKY@users.noreply.github.com>
2022-02-06 18:20:35 +0500

    upd

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-
 $( ".js-accordion" ).accordion({
   collapsible: true,
   active: true,
@@ -15,6 +14,7 @@ $( ".js-accordion" ).accordion({
   heightStyle: 'content'
 });
 
+// burger
 window.addEventListener('DOMContentLoaded', function () {
   document.querySelector("#burger").addEventListener('click', function () {
     document.querySelector('#menu').classList.toggle('is-active')
@@ -36,5 +36,25 @@ window.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('DOMContentLoaded', function () {
   document.querySelector("#btn2").addEventListener('click', function () {
     document.querySelector('#search').classList.toggle('is-active')
+  })
+})
+
+//tabs
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.working__btn').forEach(function(tabsBtn) {
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+      console.log(path)
+
+      document.querySelectorAll('.working__block').forEach(function (tabContent) {
+        tabContent.classList.remove('working__block_active')
+      })
+
+      document.querySelectorAll('.working__btn').forEach(function (btnActive) {
+        btnActive.classList.remove('working__btn_active')
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('working__block_active')
+      document.querySelector(`[data-path="${path}"]`).classList.add('working__btn_active')
+    })
   })
 })
